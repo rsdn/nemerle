67f1c6443137dbccb261b2164cfe69783e311aee
rzyj <rzyj@gmail.com>
2003-10-09 20:49:58 +0000

    Nemerle mode has been changed to switch to utf-8 automatically.  Syntax colouring improvements have been made.  Indentation framework has been written.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@186 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- aux/nemerle.el
+++ aux/nemerle.el
@@ -30,6 +30,12 @@
 
 ;;; Change Log:
 
+;; 2003-10-09 rzyjontko <rzyj@plusnet.pl>
+;;   * nemerle mode automatically sets file coding system to utf-8
+;;   * syntax table changes
+;;   * more colours
+;;   * indentation framework
+
 ;; 2003-10-05 rzyjontko <rzyj@plusnet.pl>
 ;;   * initial version
 
@@ -60,6 +66,9 @@
   "This hook is run when nemerle-mode is loaded, or a new nemerle-mode
 buffer created.  This is a good place to put your customizations.")
 
+(defvar nemerle-indent-level 4
+  "Indentation of blocks in nemerle.")
+
 (unless nemerle-mode-map
   (let ((map (make-sparse-keymap)))
     ;; this is where to put keybindings like this
@@ -69,41 +78,97 @@ buffer created.  This is a good place to put your customizations.")
 (unless nemerle-font-lock-keywords
   (setq nemerle-font-lock-keywords
 	(list
+	 ;; comments
 	 '("//.*" 0 font-lock-comment-face)
 	 '("(\\*+\\(\\([^\\*]\\)\\|\\(\\*[^)]\\)\\)+\\*+)" 
 	   0 font-lock-comment-face)
+	 
+	 ;; strings
 	 '("\"[^\"]*\"" 0 font-lock-string-face)
+
+	 ;; keywords
 	 '("\\b\\(_\\|abstract\\|and\\|as\\|base\\|class\\|const\\|else\\|enum\\|extends\\|extern\\|field\\|finally\\|fun\\|if\\|implements\\|in\\|interface\\|internal\\|let\\|letfun\\|match\\|method\\|namespace\\|new\\|null\\|open\\|out\\|private\\|protected\\|public\\|raise\\|record\\|ref\\|sealed\\|struct\\|then\\|this\\|try\\|tymatch\\|type\\|value\\|variant\\|volatile\\|where\\|with\\)\\b"
 	   0 font-lock-keyword-face)
+
+	 ;; types
 	 '("\\b\\(void\\|int\\|char\\|float\\|bool\\|string\\|option\\|list\\|object\\)\\b"
 	   0 font-lock-type-face)
+	 '("\\btype\\s +\\(\\w+\\)" 1 font-lock-type-face)
+
+	 ;; constants
 	 '("\\b\\(false\\|true\\)\\b" 0 font-lock-constant-face)
+
+	 ;; variables
+	 '("``\\w+``" 0 font-lock-variable-name-face)
+	 '("'\\w+" 0 font-lock-variable-name-face)
+	 '("\\b\\(\\w+\\)\\ *:" 1 font-lock-variable-name-face)
 	 '("\\bmatch\\s +\\(\\w+\\)\\s +with\\b" 1 font-lock-variable-name-face)
 	 '("\\blet\\s +\\(\\w+\\)" 1 font-lock-variable-name-face)
-	 '("\\b\\(class\\|variant\\|open\\)\\s +\\(\\w+\\)" 2 font-lock-function-name-face)
-	 '("\\b\\(let\\)?fun\\s *\\((.*)\\s *\\)?\\(\\w+\\)" 
-	   3 font-lock-function-name-face))))
+
+	 ;; functions
+	 '("\\b\\(class\\|variant\\|open\\|interface\\|namespace\\|extends\\|implements\\)\\s +\\(\\w+\\)" 
+	   2 font-lock-function-name-face)
+	 '("\\b\\(\\(let\\)?fun\\|method\\)\\b\\s *\\(([^)]*)\\s *\\)?\\(\\w+\\)" 
+	   4 font-lock-function-name-face))))
 
 (unless nemerle-mode-syntax-table
   (setq nemerle-mode-syntax-table (copy-syntax-table c-mode-syntax-table))
   
+  (modify-syntax-entry ?\{ "(}"   nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\} "){"   nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\[ "(]"   nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\] ")["   nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\" ".\""  nemerle-mode-syntax-table)
   (modify-syntax-entry ?\, "."    nemerle-mode-syntax-table)
-  (modify-syntax-entry ?\' "."    nemerle-mode-syntax-table)
-  (modify-syntax-entry ?\/ "."    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\` "."    nemerle-mode-syntax-table)
   (modify-syntax-entry ?\( "()1"  nemerle-mode-syntax-table)
   (modify-syntax-entry ?\) ")(4"  nemerle-mode-syntax-table)
-  (modify-syntax-entry ?\* ". 23" nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\* "w 23" nemerle-mode-syntax-table)
   (modify-syntax-entry ?\? "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\= "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\< "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\> "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\@ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\^ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\| "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\& "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\+ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\- "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\/ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\$ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\% "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\! "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\~ "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\. "w"    nemerle-mode-syntax-table)
+  (modify-syntax-entry ?\: "w"    nemerle-mode-syntax-table)
   (modify-syntax-entry ?\_ "w"    nemerle-mode-syntax-table))
 
 
 
+(defun nemerle-calculate-indentation ()
+  "Calculates indentation level for the current line."
+  (current-indentation))
+
+
 (defun nemerle-indent-line ()
-  )  
+  "Indent current line of nemerle code."
+  (interactive)
+  (let ((level (nemerle-calculate-indentation)))
+    (save-excursion
+      (beginning-of-line)
+      (skip-chars-forward " \t")
+      (if (< level (current-indentation))
+	  (delete-horizontal-space))
+      (indent-to level))
+    (if (< (current-column) (current-indentation))
+	(skip-chars-forward " \t"))))
+		   
 
 
 (defun nemerle-comment-indent ()
-  )
+  "Indent current line of nemerle comment."
+  (interactive)
+  0)
 
 
 (defun nemerle-mode ()
@@ -138,8 +203,10 @@ Mode map
   (setq comment-end " *)")
 
   (make-local-variable 'comment-start-skip)
-  (setq comment-start-skip "(\\*+[ \t]*")
+  (setq comment-start-skip "(\\*+[ \t]*\\|//+ *")
 
+  (setq buffer-file-coding-system 'utf-8)
+  
   (run-hooks 'nemerle-mode-hook))
 
 ;;; nemerle.el ends here
