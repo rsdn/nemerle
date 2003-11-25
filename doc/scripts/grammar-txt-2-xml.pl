1c20f5c3431126f23c988c0db2b2990912724e9b
malekith <malekith@gmail.com>
2003-11-25 20:27:22 +0000

    Do a s/TYVAR/TYPE_VARIABLE/g.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@529 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- doc/scripts/grammar-txt-2-xml.pl
+++ doc/scripts/grammar-txt-2-xml.pl
@@ -44,7 +44,7 @@ print "<rule name=\"$rule\">\n";
 	s/>/&gt;/g;
 	$_ = " $_ ";
 		s/"([^"]+)"/<terminal name="$1" \/>/g;
-		s/(IDENTIFIER|STRING_LITERAL|NUMBER_LITERAL|CHARACTER_LITERAL|TYVAR)/<special-terminal name="$1" \/>/g;
+		s/(IDENTIFIER|STRING_LITERAL|NUMBER_LITERAL|CHARACTER_LITERAL|TYPE_VARIABLE)/<special-terminal name="$1" \/>/g;
 	for($i=0;$i<50;$i++) {
 		s/\s\{\s/ <repeat> /;
 		s/\s\}\s/ <\/repeat> /;
