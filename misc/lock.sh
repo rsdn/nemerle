2dc46efca8a0130f758a03e84c48a77e69de3ba2
nazgul <nazgul@gmail.com>
2003-12-21 17:55:17 +0000

    Release lock. Rewrite call quotation in itself.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@865 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/lock.sh
+++ misc/lock.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 svn up ../boot/lock.txt
 if test $(tail -n 1 ../boot/lock.txt | awk '{print $1 }';) = unlocked; then 
-  echo "" >> lock.txt
-  echo $(id -un)":" >> lock.txt
-  echo "  locked "$(date) >> lock.txt
-  echo "    -- "$1 >> lock.txt
-  if svn commit ../boot/lock.txt -m "Set lock"; then
+  echo "" >> ../boot/lock.txt
+  echo $(id -un)":" >> ../boot/lock.txt
+  echo "  locked "$(date) >> ../boot/lock.txt
+  echo "    -- "$1 >> ../boot/lock.txt
+  if svn commit ../boot/lock.txt -m "Set lock. $1"; then
     echo OK: set lock succesful;
   else
     echo FAIL: something went wrong;
