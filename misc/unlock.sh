3c5841e855c367f9026a97b6a1bf67800d914bf2
nazgul <nazgul@gmail.com>
2003-12-21 15:55:06 +0000

    Release lock. Fix script.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@857 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/unlock.sh
+++ misc/unlock.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 svn up ../boot/lock.txt
-if ! test -z $(tail -n 2 ../boot/lock.txt | grep locked); then 
-  echo "  unlocked "$(date) >> lock.txt
-#  msg="Release lock. "$1
+if test -n $(tail -n 2 ../boot/lock.txt | grep locked | awk '{print $1}'); then 
+  echo "  unlocked "$(date) >> ../boot.txt
   cd ..
   if svn commit -m "Release lock. $1"; then
     echo OK: release lock succesful;
-  else
-    echo FAIL: something went wrong;
-  fi
+ else
+   echo FAIL: something went wrong;
+ fi
 else echo FAIL: project is not locked; fi
