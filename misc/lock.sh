8093b68e6bb28988b22afc89da90b0db537306d9
nazgul <nazgul@gmail.com>
2003-12-21 23:40:43 +0000

    match (e) {..  } is now possible
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@876 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/lock.sh
+++ misc/lock.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 svn up ../boot/lock.txt
 if test $(tail -n 1 ../boot/lock.txt | awk '{print $1 }';) = unlocked; then 
+  cp ../boot/lock.txt locktmp.txt
   echo "" >> ../boot/lock.txt
   echo $(id -un)":" >> ../boot/lock.txt
   echo "  locked "$(date) >> ../boot/lock.txt
   echo "    -- "$1 >> ../boot/lock.txt
   if svn commit ../boot/lock.txt -m "Set lock. $1"; then
     echo OK: set lock succesful;
+    rm -f locktmp.txt;
   else
     echo FAIL: something went wrong;
+    mv -f locktmp.txt ../boot/lock.txt;
   fi
 else echo FAIL: project is already locked; fi
 
--- misc/unlock.sh
+++ misc/unlock.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 svn up ../boot/lock.txt
 if test -n $(tail -n 2 ../boot/lock.txt | grep locked | awk '{print $1}'); then 
+  cp ../boot/lock.txt locktmp.txt
   echo "  unlocked "$(date) >> ../boot/lock.txt
   cd ..
   if svn commit -m "Release lock. $1"; then
     echo OK: release lock succesful;
+    rm -f locktmp.txt;
  else
    echo FAIL: something went wrong;
+   mv -f locktmp.txt ../boot/lock.txt;
  fi
 else echo FAIL: project is not locked; fi
