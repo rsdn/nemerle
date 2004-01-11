e386f33ce618c08fcd499ae4acca220b614df7f9
nazgul <nazgul@gmail.com>
2004-01-11 17:58:10 +0000

    Add float support in printf. Make some progress in lambda quotation, there is still problem with nonexistant names - redigning E_lambda needed.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@1068 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/lock.sh
+++ misc/lock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 svn up ../boot/lock.txt
-if test $(tail -n 1 ../boot/lock.txt | awk '{print $1 }';) = unlocked; then 
+if test $(tail -n 1 ../boot/lock.txt | awk '{print $1 }';) = "unlocked,"; then 
   cp ../boot/lock.txt locktmp.txt
   echo "" >> ../boot/lock.txt
   echo $(id -un)":" >> ../boot/lock.txt
-  echo "  locked "$(date) >> ../boot/lock.txt
+  echo "  locked, "$(date) >> ../boot/lock.txt
   echo "    -- "$1 >> ../boot/lock.txt
   if svn commit ../boot/lock.txt -m "Set lock. $1"; then
     echo OK: set lock succesful;
--- misc/unlock.sh
+++ misc/unlock.sh
@@ -2,7 +2,7 @@
 svn up ../boot/lock.txt
 if test -n $(tail -n 2 ../boot/lock.txt | grep locked | awk '{print $1}'); then 
   cp ../boot/lock.txt locktmp.txt
-  echo "  unlocked "$(date) >> ../boot/lock.txt
+  echo "  unlocked, "$(date) >> ../boot/lock.txt
   cd ..
   if svn commit -m "Release lock. $1"; then
     echo OK: release lock succesful;
