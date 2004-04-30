53b34eb5c9fadc86e66a67d8c2ad6290990f79c1
malekith <malekith@gmail.com>
2004-04-30 13:24:42 +0000

    Don't put photo in Windows dist.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@2289 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/packages/msi/makedist.sh
+++ misc/packages/msi/makedist.sh
@@ -6,5 +6,5 @@ mkdir -p dist/{bin,html}
 cp $top/boot/*.{exe,dll} dist/bin
 make -C $top/doc www
 tar zxf $top/doc/nemerle-web.tar.gz -C dist/html
-rm -r dist/html/{*.{gz,ps},course}
+rm -r dist/html/{*.{gz,ps},course,styles/the-nemerlies.jpg}
 cp License.rtf dist
