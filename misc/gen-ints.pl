488d82d1376e3149330a1a1948435295fafe20d8
malekith <malekith@gmail.com>
2004-05-19 11:48:08 +0000

    Add floating point types to integertypes.n (rename pending). Use them in constant folder. There are still some problems with this.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@2478 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/gen-ints.pl
+++ misc/gen-ints.pl
@@ -1,17 +1,43 @@
 #!/usr/bin/perl
 
 sub inttype {
-my ($long, $short, $signed) = @_;
+my ($long, $short, $signed, $int, $suff) = @_;
 print <<EOF
-  class $long : IIntegerType {
+  class $long : INumericType {
     public Zero : object
     {
-      get { 0 } 
+      get { 0$suff } 
     }
     
     public One : object
     {
-      get { 1 }
+      get { 1$suff }
+    }
+
+    public IsInteger : bool
+    {
+EOF
+;
+
+if ($int) {
+print "      get { true }\n";
+} else {
+print "      get { false }\n";
+}
+print <<EOF
+    }
+    
+    public IsSigned : bool
+    {
+EOF
+;
+
+if ($signed) {
+print "      get { true }\n";
+} else {
+print "      get { false }\n";
+}
+print <<EOF
     }
     
     public SystemType : System.Type {
@@ -28,9 +54,19 @@ print <<EOF
         | "*" => x * y
         | "/" => x / y
         | "%" => x % y
+EOF
+;
+if ($int) {
+print <<EOF
         | "%&" => x %& y
         | "%|" => x %| y
-        | _ => Util.ice ("invalid $short operator `" + name + "'")
+EOF
+;
+}
+print <<EOF
+        | _ => 
+	  null
+	  // Util.ice ("invalid $short operator `" + name + "'")
       }
     }
 
@@ -45,7 +81,9 @@ EOF
 print "        | \"-\" => -x\n" if ($signed);
 print <<EOF
         //| "~" => ~x
-        | _ => Util.ice ("invalid $short operator `" + name + "'")
+        | _ =>
+	  null
+	  // Util.ice ("invalid $short operator `" + name + "'")
       }
     }
 
@@ -53,6 +91,11 @@ print <<EOF
     {
       | L_$short (x) => (x : object)
       | _ => null
+    }
+
+    public ToLiteral (x : object) : Literal
+    {
+      L_$short ((x :> System.$long))
     }
 
     public this () {}
@@ -63,12 +106,15 @@ EOF
 ;}
 
 print "// Begin generated code\n";
-inttype ("SByte", "sbyte", 1);
-inttype ("Byte", "byte", 0);
-inttype ("Int16", "short", 1);
-inttype ("UInt16", "ushort", 0);
-inttype ("Int32", "int", 1);
-inttype ("UInt32", "uint", 0);
-inttype ("Int64", "long", 1);
-inttype ("UInt64", "ulong", 0);
+inttype ("SByte", "sbyte", 1, 1, "B");
+inttype ("Byte", "byte", 0, 1, "UB");
+inttype ("Int16", "short", 1, 1, "S");
+inttype ("UInt16", "ushort", 0, 1, "US");
+inttype ("Int32", "int", 1, 1, "");
+inttype ("UInt32", "uint", 0, 1, "U");
+inttype ("Int64", "long", 1, 1, "L");
+inttype ("UInt64", "ulong", 0, 1, "UL");
+
+inttype ("Single", "float", 1, 0, ".0F");
+inttype ("Double", "double", 1, 0, ".0");
 print "// End generated code\n";
