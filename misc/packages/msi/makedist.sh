a4d4668fc8685b85fa3f118df3befe90bb4cdad4
malekith <malekith@gmail.com>
2004-05-30 16:54:32 +0000

    Prepare 0.1.3 release.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@2563 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/packages/msi/makedist.sh
+++ misc/packages/msi/makedist.sh
@@ -6,5 +6,6 @@ mkdir -p dist/{bin,html}
 cp $top/boot/*.{exe,dll} dist/bin
 make -C $top/doc www
 tar zxf $top/doc/nemerle-web.tar.gz -C dist/html
-rm -r dist/html/{*.{gz,ps},course,styles/the-nemerlies.jpg}
+rm -r dist/html/{*.{gz,ps},course,images}
+rm -f dist/bin/true.exe
 cp License.rtf dist
