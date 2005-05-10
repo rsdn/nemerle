e17e7467216ed2ba767857b455a0e120141a087d
rzyj <rzyj@gmail.com>
2005-05-10 17:10:17 +0000

    Next try of fixing comments for XEmacs 21.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@4980 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/nemerle.el
+++ misc/nemerle.el
@@ -208,7 +208,7 @@ buffer created.  This is a good place to put your customizations.")
   (modify-syntax-entry ?\' "."  nemerle-mode-syntax-table)
   (cond
    ;; XEmacs 21
-   ((not (listp c-emacs-features))
+   ((not (boundp c-emacs-features))
     (modify-syntax-entry ?/  ". 124b" nemerle-mode-syntax-table)
     (modify-syntax-entry ?*  ". 23"   nemerle-mode-syntax-table))
    ;; XEmacs 19 & 20
