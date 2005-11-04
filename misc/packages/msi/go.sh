2b74a6887ce75d7c2687acf0f283c81d605b1ca1
malekith <malekith@gmail.com>
2005-11-04 12:43:20 +0000

    Rename MSI file after build.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@5883 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- misc/packages/msi/go.sh
+++ misc/packages/msi/go.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+set -e
 
 TMP=/tmp 
 TEMP=/tmp
 DIR=`dirname $0`
 NANT=`grep "NANT " ./$DIR/../../../config.mak | sed 's/.*=//g'`
 command=eval $NANT -t:net-2.0 -v
-$command 
+$command
+version=$(grep '"msi.version"' msi.build | sed -e 's/.*value="//; s/".*//')
+chmod 644 Nemerle.msi
+mv Nemerle.msi nemerle-$version.msi
