b653a8f03f56468d3812609b0ab79650d33f07bb
kliss <kliss@gmail.com>
2008-02-24 14:18:35 +0000

    Make Nemerle extension methods usable from C# code (add additinal attribute System.Core.CompilerServices.ExtensionMethodAttribute) Eliminate usage of all things from lib/oldapi.n (Nemerle.IEnumerable['a] etc)
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@7893 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- Build.cmd
+++ Build.cmd
@@ -2,10 +2,10 @@
 
 IF NOT "%PROCESSOR_ARCHITECTURE%" == "x86" goto b64
 IF NOT "%PROCESSOR_ARCHITEW6432%" == "" goto b64
-set MSBuild=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe
+set MSBuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe
 goto b32
 :b64
-set MSBuild=%SystemRoot%\Microsoft.NET\Framework64\v2.0.50727\MSBuild.exe
+set MSBuild=%SystemRoot%\Microsoft.NET\Framework64\v3.5\MSBuild.exe
 :b32
 
 set errors=no
