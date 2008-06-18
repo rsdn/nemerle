383eef4fca96db6d9170070d41fc74b63914e823
vladd2 <vc@rsdn.ru>
2008-06-18 19:16:33 +0000

    I rewrite string literal subsystem. Now string literal not glue in lexer. Instead it concatenate in parsing stage. It need for automated refactoring reason. Otherwise we can't get precision in AST location. Furthermore many bags was fixed. Sorry for big commit. I work not haw Internet connection in annual leave.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8045 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- ncc/testsuite/positive.cmd
+++ ncc/testsuite/positive.cmd
@@ -1,5 +1,6 @@
 IF exist positive.results del positive.results
 tests.exe -d:positive -p  "-nowarn:10003 -def:RUNTIME_MS" -s
+rem -debugger
 rem  > positive.results
 rem  type positive.results
 echo ----------------------------------------
