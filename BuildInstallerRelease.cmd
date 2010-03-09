f95b850b8ff1c6456dc6c8535a5554efc5770dec
NN <NN1436401@gmail.com>
2010-03-09 07:35:00 +0000

    Simplification. Not needed to copy files to Program Files! Take advantage of $Nemerle environment variable which is used in Linq\Macro\Linq.nproj and Tests.nproj to import MSBuild targets.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8573 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- BuildInstallerRelease.cmd
+++ BuildInstallerRelease.cmd
@@ -1,35 +1,19 @@
 @echo off
 
-rem Set Type to Release, NoPause to true
 set Type=Release
 set NoPause=true
+set Nemerle=%~dp0\boot
 
-rem Build MSBuild Task
+rem Build MSBuild task
 call Build-MSBuildTask.cmd
 
-set ProgFiles=%ProgramFiles(x86)%
-if "%ProgFiles%" == "" (
-    set ProgFiles=%ProgramFiles%
-)
-
-rem Create %ProgFiles%\Nemerle
-rd /s /q "%ProgFiles%\Nemerle" > nul
-md "%ProgFiles%\Nemerle"
-
-rem Copy needed files to %ProgFiles%\Nemerle
-rem 1. Copy from boot
-
-copy /y boot\ncc.exe /b "%ProgFiles%\Nemerle"
-copy /y boot\ncc.exe /b "%ProgFiles%\Nemerle"
-copy /y boot\Nemerle.dll /b "%ProgFiles%\Nemerle"
-copy /y boot\Nemerle.Compiler.dll /b "%ProgFiles%\Nemerle"
-copy /y boot\Nemerle.Macros.dll /b "%ProgFiles%\Nemerle"
-copy /y boot\Nemerle.MSBuild.Tasks.dll /b "%ProgFiles%\Nemerle"
-
-rem 2. Copy MSBuild targets
-copy "tools\msbuild-task\Nemerle.MSBuild.targets" /b "%ProgFiles%\Nemerle"
+rem Copy MSBuild targets to boot
+copy "tools\msbuild-task\Nemerle.MSBuild.targets" /b "boot\"
 
 rem Build Installer
 call BuildInstallerFull.cmd
 
 rem Done
+set Type=
+set NoPause=
+set Nemerle=
