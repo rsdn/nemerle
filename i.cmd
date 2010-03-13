a14c219f67165b39cd2270c5593fdd77f50e5a54
VladD2 <vc@rsdn.ru>
2010-03-13 03:08:02 +0000

    Build process has improved.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8603 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- i.cmd
+++ i.cmd
@@ -1,3 +1,3 @@
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild.exe"
-%MSBuild% NemerleAll.nproj /target:DevBuildFast /p:Configuration=Debug 
+%MSBuild% NemerleAll.nproj /target:DevBuildFast /p:Configuration=Debug /verbosity:n
 pause
