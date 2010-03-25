aaab0b60f102f5862b2355185e308a6f47c88d4f
hardcase <hardcaseminator@gmail.com>
2010-03-25 14:45:03 +0000

    [MSBuild] Nemerle.MSBuild.Tasks.dll written in C# is used to build all projects.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8665 c8a6711f-211a-0410-a8d5-2f220496d6d1

---
+++ tools/msbuild-task/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nemerle Language MSBuild Integration Library")]
+[assembly: AssemblyCompany("rsdn.ru, nemerle.org")]
+[assembly: AssemblyProduct("Nemerle Language Compiler")]
+[assembly: AssemblyCopyright("Copyright @ rsdn.ru 2006-2010, nemerle.org 2006")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+//FIXME: Take assembly version from SVN
+[assembly: AssemblyVersion("1.0.0.0")]
