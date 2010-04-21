dbf3c6afccd03f373c5390273cc140dc4a90e5b5
NN <NN1436401@gmail.com>
2010-04-21 08:59:46 +0000

    Simple macro to make overload functions.
    The macro is not perfect.
    There is one mutable and no support for breaking default arguments.
    
    The discussion is here
    http://rsdn.ru/forum/nemerle/2906649.flat.aspx#2906649
    
    Any changes are acceptable :)
    
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8780 c8a6711f-211a-0410-a8d5-2f220496d6d1

---
+++ snippets/overload/cstest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DefaultArgumentsTester;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Class1().F();
+            new Class1().F(1);
+            new Class1().F(2, "a");
+            new Class1().F(3, "b", 3);
+
+            new Class1().F2('q', 5, "d");
+            new Class1().F2('q', 6, "e", 7.5);
+        }
+    }
+}
