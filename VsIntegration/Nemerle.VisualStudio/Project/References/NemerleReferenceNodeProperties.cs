bc780354a8797107dd1725cbacf3c992c98e8dae
VladD2 <vc@rsdn.ru>
2010-06-07 13:12:08 +0000

    The forgotten file.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@8917 c8a6711f-211a-0410-a8d5-2f220496d6d1

---
+++ VsIntegration/Nemerle.VisualStudio/Project/References/NemerleReferenceNodeProperties.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Project;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Nemerle.VisualStudio.Project.References
+{
+	[CLSCompliant(false), ComVisible(true)]
+	public class NemerleReferenceNodeProperties : ReferenceNodeProperties
+	{
+		public NemerleReferenceNodeProperties(HierarchyNode node)
+			: base(node)
+		{
+		}
+
+		[LocDisplayName("HintPath")]
+		[SRCategoryAttribute("Misc")]
+		//[SRDescriptionAttribute("ssssssssssss")]
+		[Browsable(true)]
+		[AutomationBrowsable(true)]
+		public string HintPath
+		{
+			get
+			{
+				var value = Node.ItemNode.Item.GetMetadata("HintPath");
+				return value;
+			}
+		}
+	}
+}
