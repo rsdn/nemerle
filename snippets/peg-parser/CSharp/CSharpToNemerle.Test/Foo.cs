4edb1ad4bd2a06045d82c6778064fe4a76097f50
hardcase <hardcaseminator@gmail.com>
2010-08-31 13:13:50 +0000

    [PEG, C#] Work on C# AST converter. Fix bug in C# parser.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9104 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
+++ snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Console;
 
 namespace CSharpToNemerle.Test
 {
   public class Foo<T> where T : new()
   {
-    public void DoSomething()
+    public void DoSomething(int x, string p = "class: ")
     {
-      Console.WriteLine("Foo!!!");
+      if(x > 0)
+        System.Console.WriteLine("x is greater than 0.");
+      WriteLine("Generic " + p + typeof(T).FullName);
+    }
+  }
+
+  partial static class Program 
+  {
+    void TestPartial()
+    {
+      Console.WriteLine("Partial works!");
     }
   }
 }
