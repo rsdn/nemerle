69af11bd83ade3fcf46e797d2242db10312cef70
hardcase <hardcaseminator@gmail.com>
2010-08-31 17:50:26 +0000

    [PEG, C#] Work on C# AST converter.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9108 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
+++ snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
@@ -3,6 +3,14 @@ using System.Console;
 
 namespace CSharpToNemerle.Test
 {
+  public delegate T X<T>(int a, T b) where T : class;
+
+  public enum A {
+    A1 = 10,
+    A2,
+    A3
+  }
+
   public class Foo<T> where T : new()
   {
     public void DoSomething(int x, string p = "class: ")
