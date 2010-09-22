cfe13425d038d55373f1f3d5b080d11d799675be
hardcase <hardcaseminator@gmail.com>
2010-09-22 11:58:39 +0000

    [CSharpParser] Fix conversion unicode escapes (still need to handle exceptions).
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9202 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
+++ snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
@@ -172,5 +172,12 @@ namespace CSharpToNemerle.Test
       Console.WriteLine(objX is int ? "objX is int" : "objX is not int");
       Console.WriteLine(objX is int? ? "objX is int?" : "objX is not int?");
     }
+
+    void TestUnicodeSeq()
+    {
+      Console.WriteLine((int)'\uD800' == 0xD800);
+      Console.WriteLine((int)'\uE000' == 0xE000);
+      Console.WriteLine("\U0010FFF1");
+    }
   }
 }
