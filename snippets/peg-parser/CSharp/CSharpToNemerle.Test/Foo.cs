f4510da1a22a9a94df2e9ae0cf14a2aa5fe743c3
hardcase <hardcaseminator@gmail.com>
2010-09-29 19:03:09 +0000

    [CSharp] Fix parser bugs.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9237 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
+++ snippets/peg-parser/CSharp/CSharpToNemerle.Test/Foo.cs
@@ -168,7 +168,7 @@ namespace CSharpToNemerle.Test
     {
       int? nullableX = 10;
       Console.WriteLine(nullableX.HasValue ? "nullableX has value" : "no value in nullableX");
-      object objX = nullableX;
+      global::System.Object objX = nullableX;
       Console.WriteLine(objX is int ? "objX is int" : "objX is not int");
       Console.WriteLine(objX is int? ? "objX is int?" : "objX is not int?");
     }
