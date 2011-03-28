cb14262fc7b470f3e510365c1f6a0c08d134f3e1
VladD2 <vc@rsdn.ru>
2011-03-28 02:05:59 +0000

    [IDE-2010] Fix indentation.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9717 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- snippets/VS2010/WpfHint/HintSource.cs
+++ snippets/VS2010/WpfHint/HintSource.cs
@@ -10,113 +10,113 @@ namespace WpfHint
 	internal class HintSource : IDisposable
 	{
 		public event Action Activate;
-    public HintWindow HintWindow { get; set; }
+		public HintWindow HintWindow { get; set; }
 
-    /// text editor window handle
-    public IntPtr Owner { get; private set; }
+		/// text editor window handle
+		public IntPtr Owner { get; private set; }
 
-    private InputManager _inputManager;
-    private Dispatcher   _dispatcher;
+		private InputManager _inputManager;
+		private Dispatcher   _dispatcher;
 
-    public void SubClass(IntPtr owner)
+		public void SubClass(IntPtr owner)
 		{
-      Owner         = owner;
-      _inputManager = InputManager.Current;
-      _dispatcher   = _inputManager.Dispatcher;
+			Owner = owner;
+			_inputManager = InputManager.Current;
+			_dispatcher   = _inputManager.Dispatcher;
 
-      _inputManager.PreProcessInput += OnPreProcessInputEventHandler;
-      _inputManager.EnterMenuMode   += OnEnterMenuMode;
+			_inputManager.PreProcessInput += OnPreProcessInputEventHandler;
+			_inputManager.EnterMenuMode   += OnEnterMenuMode;
 
-      //Debug.WriteLine("SubClass(): " + owner);
+			//Debug.WriteLine("SubClass(): " + owner);
 		}
 
-    void Unsubscribe()
-    {
-      _inputManager.PreProcessInput -= OnPreProcessInputEventHandler;
-      _inputManager.EnterMenuMode   -= OnEnterMenuMode;
-    }
+		void Unsubscribe()
+		{
+			_inputManager.PreProcessInput -= OnPreProcessInputEventHandler;
+			_inputManager.EnterMenuMode   -= OnEnterMenuMode;
+		}
 
 		public void UnSubClass()
 		{
-      if (_dispatcher != null)
-        _dispatcher.BeginInvoke((Action)Unsubscribe);
+			if (_dispatcher != null)
+				_dispatcher.BeginInvoke((Action)Unsubscribe);
 
-      //Debug.WriteLine("UnSubClass(): " + _owner);
+			//Debug.WriteLine("UnSubClass(): " + _owner);
 		}
 
-    #region Dispatcher handlers
+		#region Dispatcher handlers
 
-    bool MouseHoverHintWindow()
-    {
-      var pos = Win32.GetCursorPos();
+		bool MouseHoverHintWindow()
+		{
+			var pos = Win32.GetCursorPos();
 
-      Func<Window, bool> process = null; process = wnd => // local funtion :)
-      {
-        if (wnd.RestoreBounds.Contains(pos))
-          return true;
+			Func<Window, bool> process = null; process = wnd => // local funtion :)
+			{
+				if (wnd.RestoreBounds.Contains(pos))
+					return true;
 
-        foreach (Window win in wnd.OwnedWindows)
-          if (process(win))
-            return true;
+				foreach (Window win in wnd.OwnedWindows)
+					if (process(win))
+						return true;
 
-        return false;
-      };
+				return false;
+			};
 
-      Trace.Assert(HintWindow != null);
-      return process(HintWindow);
-    }
+			Trace.Assert(HintWindow != null);
+			return process(HintWindow);
+		}
 
-    void OnPreProcessInputEventHandler(object sender, PreProcessInputEventArgs e)
-    {
-      var name = e.StagingItem.Input.RoutedEvent.Name;
+		void OnPreProcessInputEventHandler(object sender, PreProcessInputEventArgs e)
+		{
+			var name = e.StagingItem.Input.RoutedEvent.Name;
 
-      switch (name)
-      {
-        case "PreviewMouseDown":
-        case "PreviewMouseUp":
-        case "MouseDown":
-        case "MouseUp":
-          if (!MouseHoverHintWindow())
-            CollActivate();
-          break;
+			switch (name)
+			{
+				case "PreviewMouseDown":
+				case "PreviewMouseUp":
+				case "MouseDown":
+				case "MouseUp":
+					if (!MouseHoverHintWindow())
+						CollActivate();
+					break;
 
-        case "PreviewKeyDown":
-        case "PreviewKeyUp":
-        case "KeyDown":
-        case "KeyUp":
-        case "LostKeyboardFocus":
-          CollActivate();
-          break;
+				case "PreviewKeyDown":
+				case "PreviewKeyUp":
+				case "KeyDown":
+				case "KeyUp":
+				case "LostKeyboardFocus":
+					CollActivate();
+					break;
 
-        case "PreviewInputReport":
-        case "InputReport":
-        case "QueryCursor":
-        case "PreviewMouseMove":
-        case "MouseMove":
-          break;
-        default:
-          Debug.WriteLine(name);
-          break;
-      }
-    }
+				case "PreviewInputReport":
+				case "InputReport":
+				case "QueryCursor":
+				case "PreviewMouseMove":
+				case "MouseMove":
+					break;
+				default:
+					Debug.WriteLine(name);
+					break;
+			}
+		}
 
-    void CollActivate()
-    {
-      if (Activate != null)
-        Activate();
-    }
+		void CollActivate()
+		{
+			if (Activate != null)
+				Activate();
+		}
 
-    void OnEnterMenuMode(object sender, EventArgs e)
-    {
-      CollActivate();
-    }
+		void OnEnterMenuMode(object sender, EventArgs e)
+		{
+			CollActivate();
+		}
 
-    #endregion Dispatcher handlers
+		#endregion Dispatcher handlers
 
-    #region Implementation of IDisposable
+		#region Implementation of IDisposable
 
 
-    ~HintSource()
+		~HintSource()
 		{
 			Dispose();
 		}
@@ -127,5 +127,5 @@ namespace WpfHint
 		}
 
 		#endregion
-  }
+	}
 }
