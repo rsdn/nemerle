3dc70088da5ea429ff89c2e4dd78e463d2d75642
NN <NN1436401@gmail.com>
2011-04-17 17:05:54 +0000

    [Build] Allow settings debug build.
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9774 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- Build-4.0.cmd
+++ Build-4.0.cmd
@@ -1,3 +1,3 @@
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 
-%MSBuild% NemerleAll.nproj /t:%* /p:Configuration=Release /tv:4.0 /p:TargetFrameworkVersion=v4.0
+%MSBuild% NemerleAll.nproj /tv:4.0 /p:TargetFrameworkVersion=v4.0 /t:%*
