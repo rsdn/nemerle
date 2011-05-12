654a11256606d31eca8656647e312eecbb6d6d89
Kochetkov Vladimir <kochetkov.vladimir@gmail.com>
2011-05-12 13:44:58 +0000

    [Release] All versions changed to 1.0
    git-svn-id: https://nemerle.googlecode.com/svn/nemerle/trunk@9827 c8a6711f-211a-0410-a8d5-2f220496d6d1

--- VsIntegration/Nemerle.VisualStudio/NemerleConstants.cs
+++ VsIntegration/Nemerle.VisualStudio/NemerleConstants.cs
@@ -26,7 +26,7 @@ namespace Nemerle.VisualStudio
 		public const string FileExtensionOnly = "n";
 		public const string ProjectExtension  = "nproj";
 		public const string ProductName       = "Nemerle Visual Studio Integration";
-		public const string ProductDetails    = "Nemerle Visual Studio Integration\r\nVersion 1.0 (Release Candidate)";
+		public const string ProductDetails    = "Nemerle Visual Studio Integration\r\nVersion 1.0";
 
 		// Attention, GUID LanguageServiceGuidString also has to be changed there:
 		// Nemerle.VsIntegration\CodeSnippets\SnippetsIndex.xml
