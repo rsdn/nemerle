ac70bebbaecd44aaf35cd71b28ffbe1bf5514e4d
VladD2 <vc@rsdn.ru>
2011-08-03 18:21:35 +0400

    [VS 2010] Improve ReferenceNodeProperties.

--- snippets/VS2010/Nemerle.VisualStudio/Project/References/NemerleReferenceNodeProperties.cs
+++ snippets/VS2010/Nemerle.VisualStudio/Project/References/NemerleReferenceNodeProperties.cs
@@ -16,74 +16,93 @@ namespace Nemerle.VisualStudio.Project.References
 		{
 		}
 
-        [SRCategoryAttribute(Microsoft.VisualStudio.Project.SR.Misc)]
-        [LocDisplayName(Microsoft.VisualStudio.Project.SR.CopyToLocal)]
-        [SRDescriptionAttribute(Microsoft.VisualStudio.Project.SR.CopyToLocalDescription)]
-        public new bool CopyToLocal
-        {
-            get
-            {
-                string copyLocal = this.GetProperty(ProjectFileConstants.Private, "False");
-                if (copyLocal == null || copyLocal.Length == 0)
-                    return false;
-                return bool.Parse(copyLocal);
-            }
-            set
-            {
-                this.SetProperty(ProjectFileConstants.Private, value.ToString());
-            }
-        }
+		[SRCategoryAttribute(Microsoft.VisualStudio.Project.SR.Misc)]
+		[LocDisplayName(Microsoft.VisualStudio.Project.SR.CopyToLocal)]
+		[SRDescriptionAttribute(Microsoft.VisualStudio.Project.SR.CopyToLocalDescription)]
+		public new bool CopyToLocal
+		{
+			get
+			{
+				string copyLocal = this.GetProperty(ProjectFileConstants.Private, "False");
+				if (copyLocal == null || copyLocal.Length == 0)
+					return false; // false by defaulte!
+				return bool.Parse(copyLocal);
+			}
+			set
+			{
+				this.SetProperty(ProjectFileConstants.Private, value.ToString());
+			}
+		}
 
-		[LocDisplayName("HintPath")]
-		[SRCategoryAttribute("Misc")]
-		//[SRDescriptionAttribute("ssssssssssss")]
+		[DisplayName("Hint Path")]
+		[Description("Hint Path with all expended properties.")]
+		[CategoryAttribute("Misc")]
 		[Browsable(true)]
 		[AutomationBrowsable(true)]
 		public string HintPath
 		{
 			get
 			{
-				var value = Node.ItemNode.Item.GetMetadataValue("HintPath");
-				return value;
+				var data = Node.ItemNode.Item.GetMetadata("HintPath");
+				return data == null ? "<No hint path available>" : data.EvaluatedValue;
 			}
 		}
 
-    [SRDescription("SpecificVersionDescription"), SRCategory("Misc"), LocDisplayName("SpecificVersion")]
-    public bool SpecificVersion
-    {
-      get
-      {
-        string property = base.GetProperty("SpecificVersion", null);
-        if (string.IsNullOrEmpty(property))
-        {
-          string property2 = base.GetProperty("Include", null);
-          return property2.IndexOf("Version=", StringComparison.Ordinal) != -1;
-        }
-        return bool.Parse(property);
-      }
-      set
-      {
-       
-        if (value)
-        {
-          AssemblyReferenceNode assemblyReferenceNode = (AssemblyReferenceNode)base.Node;
-          if (assemblyReferenceNode.ResolvedAssembly != null)
-          {
-            base.Node.ItemNode.Rename(assemblyReferenceNode.ResolvedAssembly.FullName);
-            base.SetProperty("SpecificVersion", "True");
-            return;
-          }
-        }
-        else
-        {
-          AssemblyReferenceNode assemblyReferenceNode = (AssemblyReferenceNode)base.Node;
-          if (assemblyReferenceNode.ResolvedAssembly != null)
-          {
-            base.Node.ItemNode.Rename(assemblyReferenceNode.ResolvedAssembly.Name);
-            base.SetProperty("SpecificVersion", "False");
-          }
-        }
-      }
-    }
+		[DisplayName("Unevaluated Hint Path")]
+		[Description("Hint Path without expending properties.")]
+		[CategoryAttribute("Misc")]
+		[Browsable(true)]
+		[AutomationBrowsable(true)]
+		public string UnevaluatedHintPath
+		{
+			get
+			{
+				var data = Node.ItemNode.Item.GetMetadata("HintPath");
+				return data == null ? "<No hint path available>" : data.UnevaluatedValue;
+			}
+		}
+
+		[DisplayName("Specific Version")]
+		[Description("Indicates whether this reference is to a specific version of an assembly.")]
+		[DefaultValue(false)]
+		[Category("Misc")]
+		public bool SpecificVersion
+		{
+			get
+			{
+				string property = GetProperty("SpecificVersion", null);
+				var assm = (Microsoft.VisualStudio.Project.Automation.OAAssemblyReference)Node.Object;
+				if (string.IsNullOrEmpty(property))
+				{
+					string property2 = GetProperty("Include", null);
+					return property2.IndexOf("Version=", StringComparison.Ordinal) >= 0;
+				}
+
+				return bool.Parse(property);
+			}
+
+			set
+			{
+				var assemblyReferenceNode = (AssemblyReferenceNode)base.Node;
+
+				if (value)
+				{
+					if (assemblyReferenceNode.ResolvedAssembly != null)
+					{
+						Node.ItemNode.Rename(assemblyReferenceNode.ResolvedAssembly.FullName);
+						SetProperty("SpecificVersion", "True");
+						return;
+					}
+				}
+				else
+				{
+					if (assemblyReferenceNode.ResolvedAssembly != null)
+					{
+						Node.ItemNode.Rename(assemblyReferenceNode.ResolvedAssembly.Name);
+						SetProperty("SpecificVersion", "False");
+					}
+				}
+			}
+		}
 	}
 }
