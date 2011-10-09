20a5bfda0aaddf39ff5efab4a8f47c200475b74a
Vladimir Kochetkov <kochetkov.vladimir@gmail.com>
2011-10-10 01:05:16 +0400

    All versions changed to "1.1 beta"

--- VsIntegration/Nemerle.VisualStudio/NemerleConstants.cs
+++ VsIntegration/Nemerle.VisualStudio/NemerleConstants.cs
@@ -17,8 +17,8 @@ namespace Nemerle.VisualStudio
 #endif
 
 		public const string PLKMinEdition     = "standard";
-		public const string PLKCompanyName    = "NA";
-		public const string PLKProductVersion = "1.0";
+		public const string PLKCompanyName    = "Nemerle Team";
+		public const string PLKProductVersion = "1.1";
 
 		public const string LanguageId        = "Nemerle";
 		public const string LanguageName      = "Nemerle";
@@ -26,7 +26,7 @@ namespace Nemerle.VisualStudio
 		public const string FileExtensionOnly = "n";
 		public const string ProjectExtension  = "nproj";
 		public const string ProductName       = "Nemerle Visual Studio Integration";
-		public const string ProductDetails    = "Nemerle Visual Studio Integration\r\nVersion 1.0";
+		public const string ProductDetails    = "Nemerle Visual Studio Integration\r\nVersion 1.1";
 
 		// Attention, GUID LanguageServiceGuidString also has to be changed there:
 		// Nemerle.VsIntegration\CodeSnippets\SnippetsIndex.xml
