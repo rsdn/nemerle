c03399618834d60aa7726fa554c93675fc0ceec2
VladD2 <vc@rsdn.ru>
2011-10-31 02:41:09 +0300

    Fix DevBuild2StageWithTests-4.cmd

--- DevBuild2StageWithTests-4.cmd
+++ DevBuild2StageWithTests-4.cmd
@@ -2,5 +2,5 @@ set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
 %MSBuild% NemerleAll.nproj /target:DevBuild2StageWithTests /p:Configuration=Debug /verbosity:n /tv:4.0 /p:TargetFrameworkVersion=v4.0
 rem /p:NTargetName=Build
-IF %errorlevel% == 0 call Reg-bins-2.cmd
+IF %errorlevel% == 0 call Reg-bins-2-4.0.cmd
 pause
