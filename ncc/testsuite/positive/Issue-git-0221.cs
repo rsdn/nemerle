2b8abac12f4879163c14408002e5c6f5dca95543
VladD2 <vc@rsdn.ru>
2012-01-01 15:46:28 +0300

    Closes #221.

---
+++ ncc/testsuite/positive/Issue-git-0221.cs
@@ -0,0 +1,22 @@
+﻿public struct NameOrIndexParameter
+{
+  public NameOrIndexParameter(string _name) { }
+  public NameOrIndexParameter(int _index) { }
+
+  public static implicit operator NameOrIndexParameter(string name)
+  {
+    return new NameOrIndexParameter(name);
+  }
+
+  public static implicit operator NameOrIndexParameter(int index)
+  {
+    return new NameOrIndexParameter(index);
+  }
+}
+
+public class Program
+{
+
+  static void Test(NameOrIndexParameter _x) { }
+  static void Main() { Test("a"); }
+}
