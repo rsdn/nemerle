6740686f0d7d36fa86d3fafb3bb880808e051be1
VladD2 <vc@rsdn.ru>
2012-01-01 15:02:05 +0300

    Closes #220.

---
+++ ncc/testsuite/positive/Issue-git-0220.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+  public int MappingSchema
+  {
+    [DebuggerStepThrough]
+    get { return 0; }
+    set { var _ = value; }
+  }
+
+  static void Main() { }
+}
