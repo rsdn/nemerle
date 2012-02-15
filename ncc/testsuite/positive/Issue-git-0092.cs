8e6c10991da945263759966c017835b1a6fc6821
VladD2 <vc@rsdn.ru>
2012-02-16 03:18:42 +0400

    [Compiler] Improve match typing (issue #92).

---
+++ ncc/testsuite/positive/Issue-git-0092.cs
@@ -0,0 +1,22 @@
+﻿// REFERENCE: System.Data
+
+using System.Data.SqlTypes;
+using System.Data;
+
+public class P
+{
+	public readonly static IDataReader _dataReader = null;
+
+	public static SqlString GetSqlString(int index)
+	{
+		// SqlString and string have a common subtype - IComparable. But we need must to use
+		// implicite convertion instead, as their common supertype is not compatible with the type of return value.
+		return _dataReader.IsDBNull(index)
+			? SqlString.Null // type is SqlString
+			: _dataReader.GetString(index); // type is string 
+	}
+
+	static void Main()
+	{
+	}
+}
