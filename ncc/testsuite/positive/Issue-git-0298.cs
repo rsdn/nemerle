653fcb890aec29bf3d3e8eb40c82fea7d499cbbd
VladD2 <vc@rsdn.ru>
2012-02-19 14:14:09 +0400

    [Compiler] Fix to issue #298.

--- ncc/testsuite/positive/Issue-git-0298.cs
+++ ncc/testsuite/positive/Issue-git-0298.cs
@@ -7,6 +7,12 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
+public struct NameOrIndexParameter
+{
+	public NameOrIndexParameter(string n) { _name = n; }
+	public readonly string _name;
+}
+
 public class P
 {
 	static void Main()
