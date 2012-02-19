907e7b0ee9f2fb40dced3b3b2d877cfe490f12d6
VladD2 <vc@rsdn.ru>
2012-02-19 12:37:55 +0400

    [Compiler] Closes #298.

---
+++ ncc/testsuite/positive/Issue-git-0298.cs
@@ -0,0 +1,20 @@
+﻿// REFERENCE: System.Core
+// REFERENCE: Nemerle.Linq
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+public class P
+{
+	static void Main()
+	{
+		Expression<Func<int>> mi;
+
+		mi = () => 2;
+
+		var _body = Expression.Call(null, ((MethodCallExpression)mi.Body).Method);
+	}
+}
