db1d4bdd0b9a37c7e8f2da4d13c4df0764c921b5
Gleb Golubitsky <sectoid@gnolltech.org>
2012-03-14 20:28:31 +0200

    [MSBuild.Task] Removing annoying compiler warning
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tools/msbuild-task/MSBuildTask.cs
+++ tools/msbuild-task/MSBuildTask.cs
@@ -357,6 +357,7 @@ namespace Nemerle.Tools.MSBuildTask
 			var _ = try_report("error:", log_error) || try_report("warning:", log_warning)
 					|| try_report("debug:", log_error) || try_report("hint:", log_warning)
 					|| Log.LogMessageFromText(singleLine, MessageImportance.High);
+                        _ = _; // -- warning CS0219... -- Shut up, bitch!!
 		}
 
 		protected override string GetResponseFileSwitch(string responseFilePath)
