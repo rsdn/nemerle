4ea6628c82160d3303d346aaab343f0e19f6f4a5
VladD2 <vc@rsdn.ru>
2012-08-23 20:57:08 +0400

    Work on pointers. Add support of IntPtr :> xxx* conversion.

--- snippets/Nemerle.Unsafe/UnsafeTest/Program.cs
+++ snippets/Nemerle.Unsafe/UnsafeTest/Program.cs
@@ -21,48 +21,52 @@ namespace UnsafeTest
 
     static void Test4(char* ptr)
     {
-      var c = ptr[3];
+      var c = ptr[2];
       Console.WriteLine(c.ToString());
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-      fixed (char* p = "abc")
-        Test4(p);
-
-      return;
-
-      var x = 1;
-      //typeof(int).MakePointerType()
-      //Console.WriteLine(ary[x]);
-
-      //typeof(int).MakePointerType()
+      var h = GCHandle.Alloc("qwe", GCHandleType.Pinned);
+      Test4((char*)(h.AddrOfPinnedObject()));
 
 
-      //var handle = GCHandle.Alloc(ary, GCHandleType.Pinned);
-      //int* ptr = (int*)handle.AddrOfPinnedObject();
+      //fixed (char* p = "abc")
+      //  Test4(p);
 
-      //System.Diagnostics.Trace.Assert(false);
-      //var ptr = ary;
-      fixed (int* ptr = &ary[1])
-      {
-        int* ptr2 = ptr;
-        ++ptr2;
-        Console.WriteLine(ptr2[x]);
+      //return;
 
-        //handle.Free();
-        //handle = GCHandle.Alloc(new int[] { 42, 43, 44 }, GCHandleType.Pinned);
-        //ptr = (int*)handle.AddrOfPinnedObject();
-        //Console.WriteLine(ptr[x]);
-      }
+      //var x = 1;
+      ////typeof(int).MakePointerType()
+      ////Console.WriteLine(ary[x]);
+
+      ////typeof(int).MakePointerType()
 
 
-      //var str = "abcde";
+      ////var handle = GCHandle.Alloc(ary, GCHandleType.Pinned);
+      ////int* ptr = (int*)handle.AddrOfPinnedObject();
 
-      //fixed (Type* ptr = str)
+      ////System.Diagnostics.Trace.Assert(false);
+      ////var ptr = ary;
+      //fixed (int* ptr = &ary[1])
       //{
-      //  Console.WriteLine(ptr[x]);
+      //  int* ptr2 = ptr;
+      //  ++ptr2;
+      //  Console.WriteLine(ptr2[x]);
+
+      //  //handle.Free();
+      //  //handle = GCHandle.Alloc(new int[] { 42, 43, 44 }, GCHandleType.Pinned);
+      //  //ptr = (int*)handle.AddrOfPinnedObject();
+      //  //Console.WriteLine(ptr[x]);
       //}
+
+
+      ////var str = "abcde";
+
+      ////fixed (Type* ptr = str)
+      ////{
+      ////  Console.WriteLine(ptr[x]);
+      ////}
     }
   }
 }
