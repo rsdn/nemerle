912df64a313bc2e723c761c50dcc07b1d810cdb3
NN <nn1436401@gmail.com>
2013-10-12 23:05:14 +0300

    Update build scripts for 4.5.1. Add message, since MSBuild now is shipped as part of VS and not as part of .NET.

--- DevBuild2StageWithTests-4.5.1.cmd
+++ DevBuild2StageWithTests-4.5.1.cmd
@@ -1,3 +1,12 @@
+@if not exist "%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" (
+  @echo Install Microsoft Build Tools 2013
+  @echo http://www.microsoft.com/en-us/download/details.aspx?id=40014  
+  @echo And possibly .NET 4.5.1
+  @echo http://www.microsoft.com/en-us/download/details.aspx?id=40270
+  @pause
+  @exit /b 1
+)
+
 set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 set NoPause=true
 %MSBuild% NemerleAll.nproj /target:DevBuild2StageWithTests /p:Configuration=Debug /verbosity:n /tv:4.0 /p:TargetFrameworkVersion=v4.5.1
