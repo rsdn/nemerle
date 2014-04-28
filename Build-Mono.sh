3ffb60b68c698b41abe2d01e637146c16d9cedba
Alice <cannotdebug@gmail.com>
2014-04-29 02:53:00 +0900

    Modify to can be built in Mono at least in net35

--- Build-Mono.sh
+++ Build-Mono.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]; then
   echo Parameters required.
-  echo Example 1: /p:TargetFrameworkVersion=v3.5
+  echo Example 1: /p:TargetFrameworkVersion=v3.5 /t:DevBuildFull /p:Configuration=Release  /tv:4.0
   echo Example 2: /p:TargetFrameworkVersion=v4.5 /t:Stage4 /p:Configuration=Release /tv:4.0
 else
   xbuild NemerleAll-Mono.nproj $*
