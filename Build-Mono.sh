f0f2f573afd82c273161b2c2fff0ab20494a352e
NN <nn1436401@gmail.com>
2014-05-15 17:37:47 +0300

    Remove NemerleAll-Mono.nproj. There is one merged file for both builds. Requires latest Mono from master as usual.

--- Build-Mono.sh
+++ Build-Mono.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]; then
   echo Example 1: /p:TargetFrameworkVersion=v3.5 /t:DevBuildFull /p:Configuration=Release  /tv:4.0
   echo Example 2: /p:TargetFrameworkVersion=v4.5 /t:Stage4 /p:Configuration=Release /tv:4.0
 else
-  xbuild NemerleAll-Mono.nproj $*
+  xbuild NemerleAll.nproj $*
 fi
