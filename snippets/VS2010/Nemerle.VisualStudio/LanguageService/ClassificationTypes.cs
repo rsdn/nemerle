3d9b4f72594cc47c0f3c9ee885198848f2d371e0
hardcase <hardcaseminator@gmail.com>
2014-05-22 20:01:29 +0400

    Work on highlighting.

--- snippets/VS2010/Nemerle.VisualStudio/LanguageService/ClassificationTypes.cs
+++ snippets/VS2010/Nemerle.VisualStudio/LanguageService/ClassificationTypes.cs
@@ -5,208 +5,116 @@ using Microsoft.VisualStudio.Utilities;
 
 namespace Nemerle.VisualStudio.LanguageService
 {
-	internal static class ClassificationTypes
-	{
-		public const string StringExName = "Nemerle StringEx";
+  internal static class ClassificationTypes
+  {
+    public const string StringExName = "Nemerle StringEx";
 
-		[Export, Name(StringExName)]
-		public static ClassificationTypeDefinition StringEx = null;
+    [Export, Name(StringExName)]
+    public static ClassificationTypeDefinition StringEx = null;
 
-		public const string VerbatimStringName = "Nemerle String (@ Verbatim)";
+    public const string VerbatimStringName = "Nemerle String (@ Verbatim)";
 
-		[Export, Name(VerbatimStringName), BaseDefinition(PredefinedClassificationTypeNames.String)]
-		public static ClassificationTypeDefinition VerbatimString = null;
+    [Export, Name(VerbatimStringName), BaseDefinition(PredefinedClassificationTypeNames.String)]
+    public static ClassificationTypeDefinition VerbatimString = null;
 
-		public const string VerbatimStringExName = "Nemerle StringEx (@ Verbatim)";
+    public const string VerbatimStringExName = "Nemerle StringEx (@ Verbatim)";
 
-		[Export, Name(VerbatimStringExName), BaseDefinition(StringExName)]
-		public static ClassificationTypeDefinition VerbatimStringEx = null;
+    [Export, Name(VerbatimStringExName), BaseDefinition(StringExName)]
+    public static ClassificationTypeDefinition VerbatimStringEx = null;
 
-		public const string RecursiveStringName = "Nemerle String (<# #>)";
+    public const string RecursiveStringName = "Nemerle String (<# #>)";
 
-		[Export, Name(RecursiveStringName), BaseDefinition(PredefinedClassificationTypeNames.String)]
-		public static ClassificationTypeDefinition RecursiveString = null;
+    [Export, Name(RecursiveStringName), BaseDefinition(PredefinedClassificationTypeNames.String)]
+    public static ClassificationTypeDefinition RecursiveString = null;
 
-		public const string RecursiveStringExName = "Nemerle StringEx (<# #>)";
+    public const string RecursiveStringExName = "Nemerle StringEx (<# #>)";
 
-		[Export, Name(RecursiveStringExName), BaseDefinition(StringExName)]
-		public static ClassificationTypeDefinition RecursiveStringEx = null;
+    [Export, Name(RecursiveStringExName), BaseDefinition(StringExName)]
+    public static ClassificationTypeDefinition RecursiveStringEx = null;
 
-		public const string UserTypeName = "Nemerle User Types";
+    public const string UserTypeName = "Nemerle User Types";
 
-		[Export, Name(UserTypeName)]
-		public static ClassificationTypeDefinition UserType = null;
+    [Export, Name(UserTypeName)]
+    public static ClassificationTypeDefinition UserType = null;
 
-		public const string UserDelegateTypeName = "Nemerle User Types (Delegates)";
+    public const string UserDelegateTypeName = "Nemerle User Types (Delegates)";
 
-		[Export, Name(UserDelegateTypeName)]
-		public static ClassificationTypeDefinition UserDelegateType = null;
+    [Export, Name(UserDelegateTypeName)]
+    public static ClassificationTypeDefinition UserDelegateType = null;
 
-		public const string UserEnumTypeName = "Nemerle User Types (Enums)";
+    public const string UserEnumTypeName = "Nemerle User Types (Enums)";
 
-		[Export, Name(UserEnumTypeName)]
-		public static ClassificationTypeDefinition UserEnumType = null;
+    [Export, Name(UserEnumTypeName)]
+    public static ClassificationTypeDefinition UserEnumType = null;
 
-		public const string UserInterfaceTypeName = "Nemerle User Types (Interfaces)";
+    public const string UserInterfaceTypeName = "Nemerle User Types (Interfaces)";
 
-		[Export, Name(UserInterfaceTypeName)]
-		public static ClassificationTypeDefinition UserInterfaceType = null;
+    [Export, Name(UserInterfaceTypeName)]
+    public static ClassificationTypeDefinition UserInterfaceType = null;
 
-		public const string UserValueTypeName = "Nemerle User Types (Value types)";
+    public const string UserValueTypeName = "Nemerle User Types (Value types)";
 
-		[Export, Name(UserValueTypeName)]
-		public static ClassificationTypeDefinition UserValueType = null;
+    [Export, Name(UserValueTypeName)]
+    public static ClassificationTypeDefinition UserValueType = null;
 
-		public const string QuotationName = "Nemerle Quotation";
+    public const string QuotationName = "Nemerle Quotation";
 
-		[Export, Name(QuotationName)]
-		public static ClassificationTypeDefinition Quotation = null;
+    [Export, Name(QuotationName)]
+    public static ClassificationTypeDefinition Quotation = null;
 
-		public const string HighlightOneName = "Nemerle Highlight One";
+    public const string QuotationSpliceName = "Nemerle Quotation Splice";
 
-		[Export, Name(HighlightOneName)]
-		public static ClassificationTypeDefinition HighlightOne = null;
+    [Export, Name(QuotationSpliceName)]
+    public static ClassificationTypeDefinition QuotationSplice = null;
 
-		public const string HighlightTwoName = "Nemerle Highlight Two";
+    public const string QuotationBracesName = "Nemerle Quotation Braces";
 
-		[Export, Name(HighlightTwoName)]
-		public static ClassificationTypeDefinition HighlightTwo = null;
+    [Export, Name(QuotationBracesName)]
+    public static ClassificationTypeDefinition QuotationBraces = null;
 
-		public const string ToDoCommentName = "Nemerle TODO Comment";
+    public const string HighlightOneName = "Nemerle Highlight One";
 
-		[Export, Name(ToDoCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
-		public static ClassificationTypeDefinition ToDoComment = null;
+    [Export, Name(HighlightOneName)]
+    public static ClassificationTypeDefinition HighlightOne = null;
 
-		public const string BugCommentName = "Nemerle BUG Comment";
+    public const string HighlightTwoName = "Nemerle Highlight Two";
 
-		[Export, Name(BugCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
-		public static ClassificationTypeDefinition BugComment = null;
+    [Export, Name(HighlightTwoName)]
+    public static ClassificationTypeDefinition HighlightTwo = null;
 
-		public const string HackCommentName = "Nemerle HACK Comment";
+    public const string ToDoCommentName = "Nemerle TODO Comment";
 
-		[Export, Name(HackCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
-		public static ClassificationTypeDefinition HackComment = null;
+    [Export, Name(ToDoCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
+    public static ClassificationTypeDefinition ToDoComment = null;
 
-		public const string FieldIdentifierName = "Nemerle Field Identifier";
+    public const string BugCommentName = "Nemerle BUG Comment";
 
-		[Export, Name(FieldIdentifierName)]
-		public static ClassificationTypeDefinition FieldIdentifier = null;
+    [Export, Name(BugCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
+    public static ClassificationTypeDefinition BugComment = null;
 
-		public const string EventIdentifierName = "Nemerle Event Identifier";
+    public const string HackCommentName = "Nemerle HACK Comment";
 
-		[Export, Name(EventIdentifierName)]
-		public static ClassificationTypeDefinition EventIdentifier = null;
+    [Export, Name(HackCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
+    public static ClassificationTypeDefinition HackComment = null;
 
-		public const string MethodIdentifierName = "Nemerle Method Identifier";
+    public const string FieldIdentifierName = "Nemerle Field Identifier";
 
-		[Export, Name(MethodIdentifierName)]
-		public static ClassificationTypeDefinition MethodIdentifier = null;
+    [Export, Name(FieldIdentifierName)]
+    public static ClassificationTypeDefinition FieldIdentifier = null;
 
-		public const string PropertyIdentifierName = "Nemerle Property Identifier";
+    public const string EventIdentifierName = "Nemerle Event Identifier";
 
-		[Export, Name(PropertyIdentifierName)]
-		public static ClassificationTypeDefinition PropertyIdentifier = null;
+    [Export, Name(EventIdentifierName)]
+    public static ClassificationTypeDefinition EventIdentifier = null;
 
-		// Quotation types
+    public const string MethodIdentifierName = "Nemerle Method Identifier";
 
-		public const string QuotationTextName = "Nemerle Quotation Text";
+    [Export, Name(MethodIdentifierName)]
+    public static ClassificationTypeDefinition MethodIdentifier = null;
 
-		[Export, Name(QuotationTextName)]
-		public static ClassificationTypeDefinition QuotationText = null;
+    public const string PropertyIdentifierName = "Nemerle Property Identifier";
 
-		public const string QuotationKeywordName = "Nemerle Quotation Keyword";
-
-		[Export, Name(QuotationKeywordName), BaseDefinition(PredefinedClassificationTypeNames.Keyword)]
-		public static ClassificationTypeDefinition QuotationKeyword = null;
-
-		public const string QuotationCommentName = "Nemerle Quotation Comment";
-
-		[Export, Name(QuotationCommentName), BaseDefinition(PredefinedClassificationTypeNames.Comment)]
-		public static ClassificationTypeDefinition QuotationComment = null;
-
-		public const string QuotationIdentifierName = "Nemerle Quotation Identifier";
-
-		[Export, Name(QuotationIdentifierName), BaseDefinition(PredefinedClassificationTypeNames.Identifier)]
-		public static ClassificationTypeDefinition QuotationIdentifier = null;
-
-		public const string QuotationStringName = "Nemerle Quotation String";
-
-		[Export, Name(QuotationStringName), BaseDefinition(PredefinedClassificationTypeNames.String)]
-		public static ClassificationTypeDefinition QuotationString = null;
-
-		public const string QuotationNumberName = "Nemerle Quotation Number";
-
-		[Export, Name(QuotationNumberName), BaseDefinition(PredefinedClassificationTypeNames.Number)]
-		public static ClassificationTypeDefinition QuotationNumber = null;
-
-		public const string QuotationOperatorName = "Nemerle Quotation Operator";
-
-		[Export, Name(QuotationOperatorName), BaseDefinition(PredefinedClassificationTypeNames.Operator)]
-		public static ClassificationTypeDefinition QuotationOperator = null;
-
-		public const string QuotationStringExName = "Nemerle Quotation StringEx";
-
-		[Export, Name(QuotationStringExName), BaseDefinition(StringExName)]
-		public static ClassificationTypeDefinition QuotationStringEx = null;
-
-		public const string QuotationVerbatimStringName = "Nemerle Quotation VerbatimString";
-
-		[Export, Name(QuotationVerbatimStringName), BaseDefinition(VerbatimStringName)]
-		public static ClassificationTypeDefinition QuotationVerbatimString = null;
-
-		public const string QuotationVerbatimStringExName = "Nemerle Quotation StringEx (@ Verbatim)";
-
-		[Export, Name(QuotationVerbatimStringExName), BaseDefinition(VerbatimStringExName)]
-		public static ClassificationTypeDefinition QuotationVerbatimStringEx = null;
-
-		public const string QuotationRecursiveStringName = "Nemerle Quotation String (<# #>)";
-
-		[Export, Name(QuotationRecursiveStringName), BaseDefinition(RecursiveStringName)]
-		public static ClassificationTypeDefinition QuotationRecursiveString = null;
-
-		public const string QuotationRecursiveStringExName = "Nemerle Quotation StringEx (<# #>)";
-
-		[Export, Name(QuotationRecursiveStringExName), BaseDefinition(RecursiveStringExName)]
-		public static ClassificationTypeDefinition QuotationRecursiveStringEx = null;
-
-		public const string QuotationUserTypeName = "Nemerle Quotation User Types";
-
-		[Export, Name(QuotationUserTypeName), BaseDefinition(UserTypeName)]
-		public static ClassificationTypeDefinition QuotationUserType = null;
-
-		public const string QuotationUserDelegateTypeName = "Nemerle Quotation User Types (Delegates)";
-
-		[Export, Name(QuotationUserDelegateTypeName), BaseDefinition(UserDelegateTypeName)]
-		public static ClassificationTypeDefinition QuotationUserDelegateType = null;
-
-		public const string QuotationUserEnumTypeName = "Nemerle Quotation User Types (Enums)";
-
-		[Export, Name(QuotationUserEnumTypeName), BaseDefinition(UserEnumTypeName)]
-		public static ClassificationTypeDefinition QuotationUserEnumType = null;
-
-		public const string QuotationUserInterfaceTypeName = "Nemerle Quotation User Types (Interfaces)";
-
-		[Export, Name(QuotationUserInterfaceTypeName), BaseDefinition(UserInterfaceTypeName)]
-		public static ClassificationTypeDefinition QuotationUserInterfaceType = null;
-
-		public const string QuotationUserValueTypeName = "Nemerle Quotation User Types (Value types)";
-
-		[Export, Name(QuotationUserValueTypeName), BaseDefinition(UserValueTypeName)]
-		public static ClassificationTypeDefinition QuotationUserValueType = null;
-
-		public const string QuotationToDoCommentName = "Nemerle Quotation TODO Comment";
-
-		[Export, Name(QuotationToDoCommentName), BaseDefinition(ToDoCommentName)]
-		public static ClassificationTypeDefinition QuotationToDoComment = null;
-
-		public const string QuotationBugCommentName = "Nemerle Quotation BUG Comment";
-
-		[Export, Name(QuotationBugCommentName), BaseDefinition(BugCommentName)]
-		public static ClassificationTypeDefinition QuotationBugComment = null;
-
-		public const string QuotationHackCommentName = "Nemerle Quotation HACK Comment";
-
-		[Export, Name(QuotationHackCommentName), BaseDefinition(HackCommentName)]
-		public static ClassificationTypeDefinition QuotationHackComment = null;
-	}
+    [Export, Name(PropertyIdentifierName)]
+    public static ClassificationTypeDefinition PropertyIdentifier = null;
+  }
 }
--- snippets/VS2010/Nemerle.VisualStudio/LanguageService/NemerleClassifier.cs
+++ snippets/VS2010/Nemerle.VisualStudio/LanguageService/NemerleClassifier.cs
@@ -15,6 +15,8 @@ namespace Nemerle.VisualStudio.LanguageService
     private readonly ITextBuffer _textBuffer;
     private ParseResult _parseResult;
 
+
+
     public NemerleClassifier(
       IStandardClassificationService standardClassification,
       IClassificationTypeRegistryService classificationRegistry,
@@ -86,7 +88,7 @@ namespace Nemerle.VisualStudio.LanguageService
         }
       }
 
-      WalkTokens(_parseResult.Tokens, snapshot, span.Span, result);
+      WalkTokens(_parseResult.Tokens, snapshot, span.Span, result, false);
 
       return result;
     }
@@ -126,97 +128,167 @@ namespace Nemerle.VisualStudio.LanguageService
       return result;
     }
 
-    private void WalkTokens(Token token, ITextSnapshot textSnapshot, Span span, List<ClassificationSpan> classifications)
+    private void WalkTokens(Token token, ITextSnapshot textSnapshot, Span span, List<ClassificationSpan> classifications, bool isQuotation)
     {
-      for (; token != null; token = token.Next)
+      while (token != null)
       {
         var tokenSpan = NLocationToSpan(textSnapshot, token.Location);
         if (tokenSpan.Start > span.End)
           break;
 
         if (!span.IntersectsWith(tokenSpan))
+        {
+          token = token.Next;
           continue;
+        }
 
-        if (token is Token.Operator
-          || token is Token.Comma
-          || token is Token.Semicolon)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Operator));
-        }
-        else if (token is Token.DecimalLiteral
-          || token is Token.DoubleLiteral
-          || token is Token.FloatLiteral
-          || token is Token.IntegerLiteral)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.NumberLiteral));
-        }
-        else if (token is Token.CharLiteral)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.CharacterLiteral));
-        }
-        else if (token is Token.StringLiteral)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.StringLiteral));
-        }
-        else if (token is Token.Keyword)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Keyword));
-        }
-        else if (token is Token.WhiteSpace)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.WhiteSpace));
-        }
-        else if (token is Token.Identifier
-          || token is Token.IdentifierToComplete
-          || token is Token.QuotedIdentifier)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Identifier));
-        }
-        else if (token is Token.Comment)
-        {
-          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Comment));
-        }
-        else if (token is Token.LooseGroup)
-        {
-          var group = (Token.LooseGroup)token;
-          WalkTokens(group.Child, textSnapshot, span, classifications);
-        }
-        else if (token is Token.RoundGroup)
-        {
-          var group = (Token.RoundGroup)token;
-          WalkTokens(group.Child, textSnapshot, span, classifications);
-        }
-        else if (token is Token.SquareGroup)
-        {
-          var group = (Token.SquareGroup)token;
-          WalkTokens(group.Child, textSnapshot, span, classifications);
-        }
-        else if (token is Token.BracesGroup)
-        {
-          var group = (Token.BracesGroup)token;
-          WalkTokens(group.Child, textSnapshot, span, classifications);
-        }
-        else if (token is Token.QuoteGroup)
-        {
-          var group = (Token.QuoteGroup)token;
-          WalkTokens(group.Child, textSnapshot, span, classifications);
-        }
-        else if (token is Token.Namespace)
-        {
-          var nsToken = (Token.Namespace)token;
-          WalkTokens(nsToken.KeywordToken, textSnapshot, span, classifications);
-          WalkTokens(nsToken.Body, textSnapshot, span, classifications);
-        }
-        else if (token is Token.Using)
-        {
-          var nsToken = (Token.Using)token;
-          WalkTokens(nsToken.KeywordToken, textSnapshot, span, classifications);
-          WalkTokens(nsToken.Body, textSnapshot, span, classifications);
-        }
+        token = WalkToken(tokenSpan, token, textSnapshot, span, classifications, isQuotation);
       }
     }
 
-    private static IList<ClassificationSpan> EmptyClassificationSpans = new ClassificationSpan[0];
+    private Token WalkToken(Span tokenSpan, Token token, ITextSnapshot textSnapshot, Span span, List<ClassificationSpan> classifications, bool isQuotation)
+    {
+      if (token is Token.Comma
+        || token is Token.Semicolon)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Operator));
+      }
+      else if (token is Token.Operator)
+      {
+        var op = (Token.Operator)token;
+        if (isQuotation && op.name == "$" && op.Next != null)
+        {
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Operator));
+
+          var spliceToken = op.Next;
+          var spliceTokenSpan = NLocationToSpan(textSnapshot, spliceToken.Location);
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, new Span(tokenSpan.Start, spliceTokenSpan.End - tokenSpan.Start)), _classificationRegistry.GetClassificationType(ClassificationTypes.QuotationSpliceName)));
+          return WalkToken(spliceTokenSpan, spliceToken, textSnapshot, span, classifications, false);
+        }
+        else if (isQuotation && op.name == ".." && op.Next is Token.Operator && ((Token.Operator)op.Next).name == "$" && op.Next.Next != null)
+        {
+          var op2Span = NLocationToSpan(textSnapshot, op.Next.Location);
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Operator));
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, op2Span), _standardClassification.Operator));
+
+          var spliceToken     = op.Next.Next;
+          var spliceTokenSpan = NLocationToSpan(textSnapshot, spliceToken.Location);
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, new Span(tokenSpan.Start, spliceTokenSpan.End - tokenSpan.Start)), _classificationRegistry.GetClassificationType(ClassificationTypes.QuotationSpliceName)));
+          return WalkToken(spliceTokenSpan, spliceToken, textSnapshot, span, classifications, false);
+        }
+        else
+        {
+          classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Operator));
+        }
+      }
+      else if (token is Token.DecimalLiteral
+        || token is Token.DoubleLiteral
+        || token is Token.FloatLiteral
+        || token is Token.IntegerLiteral)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.NumberLiteral));
+      }
+      else if (token is Token.CharLiteral)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.CharacterLiteral));
+      }
+      else if (token is Token.StringLiteral)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.StringLiteral));
+      }
+      else if (token is Token.Keyword)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Keyword));
+      }
+      else if (token is Token.WhiteSpace)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.WhiteSpace));
+      }
+      else if (token is Token.Identifier
+        || token is Token.IdentifierToComplete
+        || token is Token.QuotedIdentifier)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Identifier));
+      }
+      else if (token is Token.Comment)
+      {
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), _standardClassification.Comment));
+      }
+      else if (token is Token.LooseGroup)
+      {
+        var group = (Token.LooseGroup)token;
+        WalkTokens(group.Child, textSnapshot, span, classifications, isQuotation);
+      }
+      else if (token is Token.RoundGroup)
+      {
+        var group = (Token.RoundGroup)token;
+        WalkTokens(group.Child, textSnapshot, span, classifications, isQuotation);
+      }
+      else if (token is Token.SquareGroup)
+      {
+        var group = (Token.SquareGroup)token;
+        WalkTokens(group.Child, textSnapshot, span, classifications, isQuotation);
+      }
+      else if (token is Token.BracesGroup)
+      {
+        var group = (Token.BracesGroup)token;
+        WalkTokens(group.Child, textSnapshot, span, classifications, isQuotation);
+      }
+      else if (token is Token.QuoteGroup)
+      {
+        var classificationType = _classificationRegistry.GetClassificationType(ClassificationTypes.QuotationName);
+        classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, tokenSpan), classificationType));
+
+        var group = (Token.QuoteGroup)token;
+
+        var quotationType = GetQuotationType(group);
+        if (quotationType != null)
+        {
+          var quotationTypeSpan = NLocationToSpan(textSnapshot, quotationType.Location);
+          if (span.IntersectsWith(quotationTypeSpan))
+            classifications.Add(new ClassificationSpan(new SnapshotSpan(textSnapshot, quotationTypeSpan), _standardClassification.Keyword));
+        }
+
+        WalkTokens(group.Child, textSnapshot, span, classifications, true);
+      }
+      else if (token is Token.Namespace)
+      {
+        var nsToken = (Token.Namespace)token;
+        WalkTokens(nsToken.KeywordToken, textSnapshot, span, classifications, isQuotation);
+        WalkTokens(nsToken.Body, textSnapshot, span, classifications, isQuotation);
+      }
+      else if (token is Token.Using)
+      {
+        var nsToken = (Token.Using)token;
+        WalkTokens(nsToken.KeywordToken, textSnapshot, span, classifications, isQuotation);
+        WalkTokens(nsToken.Body, textSnapshot, span, classifications, isQuotation);
+      }
+
+      return token.Next;
+    }
+
+    private static Token.Identifier GetQuotationType(Token.QuoteGroup group)
+    {
+      if (group.Child is Token.LooseGroup)
+      {
+        var content = (Token.LooseGroup)group.Child;
+        if (content.Child is Token.Identifier)
+        {
+          var identifier = (Token.Identifier)content.Child;
+          if (_quotationTypes.Contains(identifier.name) && identifier.Next is Token.Operator)
+          {
+            var op = (Token.Operator)identifier.Next;
+            if (op.name == ":")
+              return identifier;
+          }
+        }
+      }
+      return null;
+    }
+
+    private static readonly HashSet<string> _quotationTypes = new HashSet<string>(new []{ "decl", "parameter", "ttype", "case" });
+
+    private static readonly IList<ClassificationSpan> EmptyClassificationSpans = new ClassificationSpan[0];
 
     private sealed class ParseResult
     {
