c6059c3ebf2c29009a1d3689955dd63c318335d0
VladD2 <vc@rsdn.ru>
2014-06-20 23:58:12 +0400

    Convert tabs to spaces.

--- snippets/VS2010/Nemerle.VisualStudio/LanguageService/ContainedLanguage/NemerleCompletionSet.cs
+++ snippets/VS2010/Nemerle.VisualStudio/LanguageService/ContainedLanguage/NemerleCompletionSet.cs
@@ -22,115 +22,115 @@ using Nemerle.Completion2;
 
 namespace Nemerle.VisualStudio.LanguageService
 {
-	internal sealed class NemerleCompletionSet : CompletionSet
-	{
-		public NemerleSource Source { get; private set; }
-		internal IVsTextView view;
+  internal sealed class NemerleCompletionSet : CompletionSet
+  {
+    public NemerleSource Source { get; private set; }
+    internal IVsTextView view;
 
-		internal NemerleCompletionSet(ImageList imageList, NemerleSource source)
-			: base(imageList, source)
-		{
-			Source = source;
-		}
+    internal NemerleCompletionSet(ImageList imageList, NemerleSource source)
+      : base(imageList, source)
+    {
+      Source = source;
+    }
 
-		public override void Init(IVsTextView textView, Declarations declarations, bool completeWord)
-		{
-			view = textView;
-			base.Init(textView, declarations, completeWord);
-		}
+    public override void Init(IVsTextView textView, Declarations declarations, bool completeWord)
+    {
+      view = textView;
+      base.Init(textView, declarations, completeWord);
+    }
 
-		public override int GetInitialExtent(out int line, out int startIdx, out int endIdx)
-		{
-			var source =  (NemerleSource)this.Source;
-			var textView = view.ToITextView();
-			var textBuffer = textView.TextBuffer;
-			var textViewLine = textView.Caret.ContainingTextViewLine;
-			var snapshot = textBuffer.CurrentSnapshot;
+    public override int GetInitialExtent(out int line, out int startIdx, out int endIdx)
+    {
+      var source =  (NemerleSource)this.Source;
+      var textView = view.ToITextView();
+      var textBuffer = textView.TextBuffer;
+      var textViewLine = textView.Caret.ContainingTextViewLine;
+      var snapshot = textBuffer.CurrentSnapshot;
 
-			var caretPos = textView.Caret.Position.BufferPosition.Position;
-			var caretSpan = new Span(caretPos, 0);
-			//textView.Li
-			ClassificationParseResult parseResult;
-			if (ParseUtil.TryParse(textBuffer, out parseResult))
-			{
-				var spanInfos = ParseUtil.GetTokenSpans(parseResult, textViewLine.Extent);
-				foreach (var spanInfo in spanInfos)
-				{
-					if (spanInfo.Span.IntersectsWith(caretSpan))
-					{
-						var loc = Utils.ToNLocation(source.FileIndex, new SnapshotSpan(snapshot, spanInfo.Span));
-						if (loc.Line != loc.EndLine)
-						{
-							Debug.Assert(false);
-						}
-						line = loc.Line - 1;
-						startIdx = loc.Column - 1;
-						endIdx = loc.EndColumn - 1;
-						return VSConstants.S_OK;
-					}
-				}
-			}
+      var caretPos = textView.Caret.Position.BufferPosition.Position;
+      var caretSpan = new Span(caretPos, 0);
+      //textView.Li
+      ClassificationParseResult parseResult;
+      if (ParseUtil.TryParse(textBuffer, out parseResult))
+      {
+        var spanInfos = ParseUtil.GetTokenSpans(parseResult, textViewLine.Extent);
+        foreach (var spanInfo in spanInfos)
+        {
+          if (spanInfo.Span.IntersectsWith(caretSpan))
+          {
+            var loc = Utils.ToNLocation(source.FileIndex, new SnapshotSpan(snapshot, spanInfo.Span));
+            if (loc.Line != loc.EndLine)
+            {
+              Debug.Assert(false);
+            }
+            line = loc.Line - 1;
+            startIdx = loc.Column - 1;
+            endIdx = loc.EndColumn - 1;
+            return VSConstants.S_OK;
+          }
+        }
+      }
 
-			int column;
-			ErrorHelper.ThrowOnFailure(this.view.GetCaretPos(out line, out column));
-			startIdx = endIdx = column;
-			return VSConstants.S_OK;
-		}
+      int column;
+      ErrorHelper.ThrowOnFailure(this.view.GetCaretPos(out line, out column));
+      startIdx = endIdx = column;
+      return VSConstants.S_OK;
+    }
 
-		public override int OnCommit(string textSoFar, int index, int selected, ushort commitChar, out string completeWord)
-		{
-			try
-			{
-				var decls = (NemerleDeclarations)Declarations;
+    public override int OnCommit(string textSoFar, int index, int selected, ushort commitChar, out string completeWord)
+    {
+      try
+      {
+        var decls = (NemerleDeclarations)Declarations;
 
-				if (decls.Result.ImportCompletion)
-				{
-					var env = decls.Result.CompletionResult.Env;
-					var elem = decls.Result.CompletionResult.CompletionList[index];
+        if (decls.Result.ImportCompletion)
+        {
+          var env = decls.Result.CompletionResult.Env;
+          var elem = decls.Result.CompletionResult.CompletionList[index];
 
-					var usingInfo = NemerleCompletionResult.CalcUsingDeclarationInfo(env, elem.Overloads[0]);
+          var usingInfo = NemerleCompletionResult.CalcUsingDeclarationInfo(env, elem.Overloads[0]);
 
-					if (!usingInfo.NeedUsing && !usingInfo.Hiden && string.IsNullOrEmpty(usingInfo.Alias))
-						return base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
+          if (!usingInfo.NeedUsing && !usingInfo.Hiden && string.IsNullOrEmpty(usingInfo.Alias))
+            return base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
 
-					if (!string.IsNullOrEmpty(usingInfo.Alias))
-					{
-						var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
-						completeWord = usingInfo.Alias + "." + completeWord;
-						return result;
-					}
+          if (!string.IsNullOrEmpty(usingInfo.Alias))
+          {
+            var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
+            completeWord = usingInfo.Alias + "." + completeWord;
+            return result;
+          }
 
-					if (usingInfo.Hiden)
-					{
-						var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
-						completeWord = usingInfo.Namespase + "." + completeWord;
-						return result;
-					}
+          if (usingInfo.Hiden)
+          {
+            var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
+            completeWord = usingInfo.Namespase + "." + completeWord;
+            return result;
+          }
 
-					if (usingInfo.NeedUsing)
-					{
-						var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
-						
-						if (result == VSConstants.S_OK)
-						{
-							var cu = Source.CompileUnit;
+          if (usingInfo.NeedUsing)
+          {
+            var result = base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
+            
+            if (result == VSConstants.S_OK)
+            {
+              var cu = Source.CompileUnit;
 
-							var line = cu != null
-								? NemerleCompletionResult.CalcUsingDeclarationInsertionLine(usingInfo.Namespase, cu) - 1
-								: 0;
-							//if (Source.CompletedFirstParse && cu == null)
-							Source.SetText(line, 0, line, 0, "using " + usingInfo.Namespase + ";" + Environment.NewLine);
-						}
+              var line = cu != null
+                ? NemerleCompletionResult.CalcUsingDeclarationInsertionLine(usingInfo.Namespase, cu) - 1
+                : 0;
+              //if (Source.CompletedFirstParse && cu == null)
+              Source.SetText(line, 0, line, 0, "using " + usingInfo.Namespase + ";" + Environment.NewLine);
+            }
 
-						return result;
-					}
-				}
-			}
-			catch (Exception)
-			{
-			}
-			return base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
-		}
-	}
+            return result;
+          }
+        }
+      }
+      catch (Exception)
+      {
+      }
+      return base.OnCommit(textSoFar, index, selected, commitChar, out completeWord);
+    }
+  }
 }
 
