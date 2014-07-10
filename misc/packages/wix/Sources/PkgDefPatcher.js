e2075a228a886b5c4503e67137612599791eb764
hardcase <hardcaseminator@gmail.com>
2014-07-10 17:14:07 +0400

    Fix and speedup VS extension deploy.

--- misc/packages/wix/Sources/PkgDefPatcher.js
+++ misc/packages/wix/Sources/PkgDefPatcher.js
@@ -95,3 +95,11 @@ function VS2013() {
   writeStream.WriteLine('"newVersion"="12.0.0.0"'                                                                                         );
   writeStream.Close();
 }
+
+function UpdateFileTimestamp() {
+  var filePath   = Session.Property("CustomActionData");
+  var fso        = new ActiveXObject("Scripting.FileSystemObject");
+  var fileStream = fso.OpenTextFile(filePath, /*ForWrite*/ 2,  /*CreateNew*/ true, /*TristateFalse*/ 0);
+  fileStream.Write("It's never too late to have a happy childhood.");
+  fileStream.Close();
+}
