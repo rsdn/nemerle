48994a167b313f57dd42d627934f2ebbd4ab11ef
hardcase <hardcaseminator@gmail.com>
2014-10-09 20:08:57 +0400

    Fix deployment of IDE extension during installer build.

--- DevBuildForCommit-4.cmd
+++ DevBuildForCommit-4.cmd
@@ -1,5 +1,5 @@
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
-%MSBuild% NemerleAll.nproj /target:DevBuildFull /p:Configuration=Debug /verbosity:n  /tv:4.0 /p:TargetFrameworkVersion=v4.0
+%MSBuild% NemerleAll.nproj /target:DevBuildFull /p:Configuration=Debug /verbosity:n /p:NInstallIntegration=True /tv:4.0 /p:TargetFrameworkVersion=v4.0
 rem  /p:TargetName=ReBuild
 pause
--- DevBuildQuick-VS2010.cmd
+++ DevBuildQuick-VS2010.cmd
@@ -1,6 +1,6 @@
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
-%MSBuild% NemerleAll.nproj /target:DevBuildQuick /p:Configuration=Debug /verbosity:n /p:NTargetName=Build  /tv:4.0 /p:TargetFrameworkVersion=v4.0
+%MSBuild% NemerleAll.nproj /target:DevBuildQuick /p:Configuration=Debug /verbosity:n /p:NTargetName=Build /p:NInstallIntegration=True /tv:4.0 /p:TargetFrameworkVersion=v4.0
 
 rem /verbosity:n /p:TargetName=Build
 IF %errorlevel% == 0 call Reg-bins-VS2010.cmd
