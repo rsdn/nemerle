3c842c7bc8fc14fc8de8c24c9bd003926ce5165a
VladD2 <vc@rsdn.ru>
2016-01-30 13:14:38 +0300

    Add CleanUp to DevBuildQuick-VS2010.cmd and DevBuildQuick-VS2012.cmd

--- DevBuildQuick-VS2012.cmd
+++ DevBuildQuick-VS2012.cmd
@@ -1,3 +1,4 @@
+IF NOT "%1" == "Build" call CleanUp.cmd
 set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 set NoPause=true
 %MSBuild% NemerleAll.nproj /target:DevBuildQuick /p:Configuration=Debug /verbosity:n /p:NTargetName=Build  /tv:4.0 /p:TargetFrameworkVersion=v4.0
