59b28607968a9bee060b42eac55c69c37d1c0e69
VladD2 <vc@rsdn.ru>
2017-12-25 11:52:16 +0300

    Added DevBuildQuick-VS2017.cmd

---
+++ DevBuildQuick-VS2017.cmd
@@ -0,0 +1,8 @@
+IF NOT "%1" == "Build" call CleanUp.cmd
+set MSBuild="%VS150COMNTOOLS%\..\..\MSBuild\15.0\Bin\MSBuild.exe"
+set NoPause=true
+%MSBuild% NemerleAll.nproj /target:DevBuildQuick /p:Configuration=Debug /verbosity:n /p:NTargetName=Build  /tv:15.0 /p:TargetFrameworkVersion=v4.0
+
+rem /verbosity:n /p:TargetName=Build
+rem IF %errorlevel% == 0 call Reg-bins-VS2015.cmd
+pause
