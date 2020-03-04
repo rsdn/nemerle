26a27b9efe9daa9877cb70b341e80158a614837d
VladD2 <vc@rsdn.ru>
2020-03-04 18:52:17 +0300

    Fix calculation of RawString.

---
+++ Build-core-with-tests.cmd
@@ -0,0 +1,4 @@
+@echo off
+title %~nx0
+rem                      Config  Target  Features
+%~dp0Build-core-base.cmd Debug   Build   "Stage1;_PegAndCSharp;_ComputationExpressions;_Async;CompilerTests;Install"
